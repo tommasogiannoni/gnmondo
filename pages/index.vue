0d33b18bfa4ade8ddf8cc3189b24431f50e3fba3
tommasogiannoni <tommaso.giannoni@yahoo.it>
2022-03-14 21:58:32 +0100

    changes

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,32 @@
 <template>
   <div class="container">
     <h1 style="margin-bottom: 10px">Giovani del nuovo mondo 👋</h1>
-    <h4 style="margin-bottom: 60px">Eventi del mese</h4>
-    <div class="container card"></div>
-    <div class="container card"></div>
-    <div class="container card"></div>
-    <div class="container card"></div>
+    <h4 style="margin-bottom: 60px; font-weight: lighter;">Eventi del mese</h4>
+    
+    <div class="" style="display: flex; justify-content: center; flex-flow: wrap;">
+
+      <b-card 
+        class="card-plus" 
+        border-variant="info" 
+        header="Info"
+        align="center"
+        >
+        <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+      </b-card>
     </div>
+</div>
   </div>
 </template>
 
 <style>
-.card {
+.card-plus {
   width: 800px;
   height: 300px;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-  text-align: center;
-  margin-bottom: 10px
+  border-color: #1C658C !important
+}
+.card-header {
+  background-color: #D8D2CB !important;
 }
 
 </style>
