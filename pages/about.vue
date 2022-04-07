0d28877b791050e218e2c2d9459404ee4c707668
tommasogiannoni <tommaso.giannoni@yahoo.it>
2022-04-07 10:52:50 +0200

    Update graphics and page Chi siamo

--- pages/about.vue
+++ pages/about.vue
@@ -1,8 +1,30 @@
 <template>
   <div>
-    <h1>Qualcosa su di noi ✨</h1>
+    <h1 style="margin-bottom: 40px;">✨Qualcosa su di noi ✨</h1>
     <div>
-      <h4 style="margin-bottom: 60px; font-weight: lighter;">Ecco i link ai nostri social</h4> 
+      <h4 style="margin-bottom: 60px; margin-left: 60px; margin-right: 60px; font-weight: lighter;">
+        <p>
+          Il gruppo <span style="font-style: italic;">Giovani Nuovo Mondo</span> nasce con lo scopo di confrontarsi 
+          sulle tematiche attuali in modo libero, proprio perché ognuno di 
+          noi è un libero cittadino del mondo, oltre che provare a combattere 
+          concretamente e giorno dopo giorno la società in cui ci troviamo. 
+        </p>
+        <p>
+          In una realtà in cui veniamo indotti a dividersi e pensare al singolo, 
+          abbiamo invece deciso di creare questa “comunità nella società” per ampliare 
+          i nostri orizzonti e aiutarsi, rimanendo quanto più uniti possibile. 
+        </p>
+        <p>  
+          Il gruppo ha come fine principale quello di far sentire ognuno di noi 
+          parte di qualcosa e creare così delle amicizie che ci possano arricchire. 
+        </p>
+        <p> 
+          Grazie ai numerosi <span style="font-style: italic;">eventi</span> che abbiamo mensilmente in programma cerchiamo 
+          di rendere partecipi tutti i componenti con attività e appuntamenti di svago 
+          differenti tra loro che riescano a tenerci uniti mettendo d’accordo quanto più i gusti di tutti.
+        </p>
+      </h4> 
+      
     </div>
   </div>
 </template>
